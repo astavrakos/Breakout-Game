a166f5c757fcdc3070e9ffc31174fc6a4d9ed137
Alexandra Stavrakos <lexistavrakos@Alexandras-MacBook-Pro.local>
2017-07-06 10:45:36 -0500

    Created brick object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -13,6 +13,7 @@ class GameScene: SKScene
 {
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
+    var brick = SKSpriteNode()
 
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
@@ -22,6 +23,7 @@ class GameScene: SKScene
         createBackground()
         makeBall()
         makePaddle()
+        makeBrick()
     }
     
     func createBackground()
@@ -77,5 +79,15 @@ class GameScene: SKScene
         paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
         paddle.physicsBody?.isDynamic = false
         addChild(paddle)
+    }
+    
+    func makeBrick()
+    {
+        brick = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 50, height: 20))
+        brick.position = CGPoint(x: frame.midX, y: frame.maxY  - 30)
+        brick.name = "brick"
+        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+        brick.physicsBody?.isDynamic = false
+        addChild(brick)
     }
 }
